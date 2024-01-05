5fcdc514bc9f1f1f806052c355e9763c1fef4db4
Labeeb Alsamwal <lalsmwl@gmail.com>
2024-01-05 04:37:13 +0400

    registration completed

--- Pydcrm/website/forms.py
+++ Pydcrm/website/forms.py
@@ -4,14 +4,14 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email address'})
-    first_name = forms.CharField(label="",max_length="255",widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'first name'})
-    last_name = forms.CharField(label="",max_length="255",widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'last name'})
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control' , 'placeholder': 'Email address'}))
+    first_name = forms.CharField(label="",max_length="255",widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'first name'}))
+    last_name = forms.CharField(label="",max_length="255",widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'last name'}))
     class Meta:
         model= User
         fields= ('username', 'first_name', 'last_name', 'email','password1', 'password2')
     def __init__(self, *args, **kwargs):
-		super(SignUpForm, self).__init__(*args, **kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
